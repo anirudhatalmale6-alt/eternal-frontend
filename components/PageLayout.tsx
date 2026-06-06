06f87a3fae1b875ec8e11ec6d1f13253d92d7126
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-06-06 08:30:52 +0000

    Match WP container: 800px max-width, 69.6% main + 29% sidebar, border-right separator

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -7,11 +7,11 @@ interface PageLayoutProps {
 export default function PageLayout({ children }: PageLayoutProps) {
   return (
     <div className="min-h-screen bg-[#0D0D0D] text-white">
-      <div className="max-w-[1170px] mx-auto px-[15px] py-[25px] flex flex-col lg:flex-row gap-[30px]">
-        <div className="w-full lg:w-[73%] min-w-0">
+      <div className="max-w-[800px] mx-auto py-[20px] flex flex-col lg:flex-row">
+        <div className="w-full lg:w-[69.6%] min-w-0 lg:float-left lg:border-r lg:border-[#570000] px-[10px]">
           {children}
         </div>
-        <aside className="w-full lg:w-[27%] flex-shrink-0">
+        <aside className="w-full lg:w-[29%] lg:float-right px-[5px]">
           <Sidebar />
         </aside>
       </div>
